776f188d2ce83a9e7716fe15874bc031ed4bc305
Yeattt <nekz.8896@gmail.com>
2023-09-13 14:51:35 -0500

    Error logger into products service added

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, BadRequestException, InternalServerErrorException } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger: Logger = new Logger('ProductsService');
+  
   async findAll() {
-    const resp = await axios.get('https://api.cataprom.com/rest/productos/CP-175');
+    try {
+      const resp = await axios.get('https://api.cataprom.com/rest/productos/CP-175');
+      return resp;
+    } catch (error) {
+      this.handleDbExceptions(error);
+    }
 
-    return resp;
+  }
+
+  private handleDbExceptions(error: any) {
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
