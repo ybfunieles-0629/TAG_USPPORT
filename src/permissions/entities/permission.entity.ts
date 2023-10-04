4194ed3af8a5690b310603b58a97d4d578bff428
Yeattt <nekz.8896@gmail.com>
2023-10-04 16:48:05 -0500

    Imports path of permissions edited

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-import { User } from 'src/users/entities/user.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity({ name: 'permissions' })
 export class Permission {
