d09a6a523070a3964dd1c9e07ea1e2bacd590e27
Yeattt <nekz.8896@gmail.com>
2023-10-09 11:17:08 -0500

    Category tag module edited

--- src/category-tag/category-tag.module.ts
+++ src/category-tag/category-tag.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoryTagService } from './category-tag.service';
 import { CategoryTagController } from './category-tag.controller';
+import { CategoryTag } from './entities/category-tag.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([CategoryTag])
+  ],
   controllers: [CategoryTagController],
   providers: [CategoryTagService],
+  exports: [TypeOrmModule, CategoryTagService]
 })
-export class CategoryTagModule {}
+export class CategoryTagModule { }
