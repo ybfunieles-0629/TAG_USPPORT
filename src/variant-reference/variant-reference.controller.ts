cf0c94b35ea71f5e8fd0b951b8378f6ffb67ceba
Yeattt <nekz.8896@gmail.com>
2023-10-10 13:26:29 -0500

    Variant reference crud added (TODO: Update method)

--- src/variant-reference/variant-reference.controller.ts
+++ src/variant-reference/variant-reference.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+
 import { VariantReferenceService } from './variant-reference.service';
 import { CreateVariantReferenceDto } from './dto/create-variant-reference.dto';
 import { UpdateVariantReferenceDto } from './dto/update-variant-reference.dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Controller('variant-reference')
 export class VariantReferenceController {
-  constructor(private readonly variantReferenceService: VariantReferenceService) {}
+  constructor(private readonly variantReferenceService: VariantReferenceService) { }
 
   @Post()
   create(@Body() createVariantReferenceDto: CreateVariantReferenceDto) {
@@ -13,22 +15,25 @@ export class VariantReferenceController {
   }
 
   @Get()
-  findAll() {
-    return this.variantReferenceService.findAll();
+  findAll(paginationDto: PaginationDto) {
+    return this.variantReferenceService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.variantReferenceService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.variantReferenceService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVariantReferenceDto: UpdateVariantReferenceDto) {
-    return this.variantReferenceService.update(+id, updateVariantReferenceDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateVariantReferenceDto: UpdateVariantReferenceDto
+  ) {
+    return this.variantReferenceService.update(id, updateVariantReferenceDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.variantReferenceService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.variantReferenceService.remove(id);
   }
 }
--- src/variant-reference/variant-reference.service.ts
+++ src/variant-reference/variant-reference.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateVariantReferenceDto } from './dto/create-variant-reference.dto';
 import { UpdateVariantReferenceDto } from './dto/update-variant-reference.dto';
+import { VariantReference } from './entities/variant-reference.entity';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class VariantReferenceService {
-  create(createVariantReferenceDto: CreateVariantReferenceDto) {
-    return 'This action adds a new variantReference';
+  private readonly logger: Logger = new Logger('VariantReferenceService');
+
+  constructor(
+    @InjectRepository(VariantReference)
+    private readonly variantReferenceRepository: Repository<VariantReference>,
+  ) { }
+
+  async create(createVariantReferenceDto: CreateVariantReferenceDto) {
+    try {
+      const variantReference = this.variantReferenceRepository.create(createVariantReferenceDto);
+
+      await this.variantReferenceRepository.save(variantReference);
+      
+      return {
+        variantReference
+      };
+    } catch (error) {
+      this.handleDbExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all variantReference`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.variantReferenceRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} variantReference`;
+  async findOne(id: string) {
+    const variantReference = await this.variantReferenceRepository.findOne({
+      where: {
+        id
+      },
+    });
+
+    if (!variantReference)
+      throw new NotFoundException(`Variant reference with id ${id} not found`);
+    
+    return {
+      variantReference
+    };
   }
 
-  update(id: number, updateVariantReferenceDto: UpdateVariantReferenceDto) {
+  async update(id: string, updateVariantReferenceDto: UpdateVariantReferenceDto) {
     return `This action updates a #${id} variantReference`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} variantReference`;
+  async remove(id: string) {
+    const { variantReference } = await this.findOne(id);
+
+    await this.variantReferenceRepository.remove(variantReference);
+
+    return {
+      variantReference
+    };
+  }
+
+  private handleDbExceptions(error: any) {
+    if (error.code === '23505' || error.code === 'ER_DUP_ENTRY')
+      throw new BadRequestException(error.sqlMessage);
+
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
