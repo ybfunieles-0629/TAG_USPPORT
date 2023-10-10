279a1d71709a91ede2987ffcbc31de302b43263c
Yeattt <nekz.8896@gmail.com>
2023-10-10 13:24:48 -0500

    Colors crud added (TODO: Update method)

--- src/colors/colors.controller.ts
+++ src/colors/colors.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { ColorsService } from './colors.service';
 import { CreateColorDto } from './dto/create-color.dto';
 import { UpdateColorDto } from './dto/update-color.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('colors')
 export class ColorsController {
-  constructor(private readonly colorsService: ColorsService) {}
+  constructor(private readonly colorsService: ColorsService) { }
 
   @Post()
   create(@Body() createColorDto: CreateColorDto) {
@@ -13,22 +14,27 @@ export class ColorsController {
   }
 
   @Get()
-  findAll() {
-    return this.colorsService.findAll();
+  findAll(
+    @Param() paginationDto: PaginationDto
+  ) {
+    return this.colorsService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.colorsService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.colorsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateColorDto: UpdateColorDto) {
-    return this.colorsService.update(+id, updateColorDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateColorDto: UpdateColorDto
+  ) {
+    return this.colorsService.update(id, updateColorDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.colorsService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.colorsService.remove(id);
   }
 }
--- src/colors/colors.service.ts
+++ src/colors/colors.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, BadRequestException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateColorDto } from './dto/create-color.dto';
 import { UpdateColorDto } from './dto/update-color.dto';
+import { Color } from './entities/color.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class ColorsService {
-  create(createColorDto: CreateColorDto) {
-    return 'This action adds a new color';
+  private readonly logger: Logger = new Logger('ColorsService');
+
+  constructor(
+    @InjectRepository(Color)
+    private readonly colorRepository: Repository<Color>,
+  ) { }
+
+  async create(createColorDto: CreateColorDto) {
+    try {
+      const color = this.colorRepository.create(createColorDto);
+
+      await this.colorRepository.save(color);
+
+      return {
+        color
+      };
+    } catch (error) {
+      this.handleDbExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all colors`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.colorRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} color`;
+  async findOne(id: string) {
+    const color = await this.colorRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!color)
+      throw new NotFoundException(`Color with id ${id} not found`);
+
+    return {
+      color
+    };
   }
 
-  update(id: number, updateColorDto: UpdateColorDto) {
+  async update(id: string, updateColorDto: UpdateColorDto) {
     return `This action updates a #${id} color`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} color`;
+  // async desactivate(id: string) {
+  //   const { color } = await this.findOne(id);
+
+  //   color.
+
+  //   return {
+  //     color
+  //   };
+  // }
+
+  async remove(id: string) {
+    const { color } = await this.findOne(id);
+
+    await this.colorRepository.remove(color);
+
+    return {
+      color
+    };
+  }
+
+  private handleDbExceptions(error: any) {
+    if (error.code === '23505' || error.code === 'ER_DUP_ENTRY')
+      throw new BadRequestException(error.sqlMessage);
+
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
