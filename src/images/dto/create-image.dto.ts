693ad81d0ebb72efaad25689419a85adbd0e69e2
Yeattt <nekz.8896@gmail.com>
2023-10-23 21:47:44 -0500

    Url property into image dto switched to optional

--- src/images/dto/create-image.dto.ts
+++ src/images/dto/create-image.dto.ts
@@ -1,8 +1,8 @@
 import { IsString } from 'class-validator';
 
 export class CreateImageDto {
-  @IsString()
-  url: string;
+  // @IsString()
+  // url: string;
 
   @IsString()
   refProduct: string;
