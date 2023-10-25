bff84a544a8b8b9fa7e6340f345f9e3c142012dd
Yeattt <nekz.8896@gmail.com>
2023-10-24 21:11:26 -0500

    Marked service prices bugs fixed

--- src/marked-service-prices/marked-service-prices.module.ts
+++ src/marked-service-prices/marked-service-prices.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarkedServicePricesService } from './marked-service-prices.service';
 import { MarkedServicePricesController } from './marked-service-prices.controller';
 import { MarkedServicePrice } from './entities/marked-service-price.entity';
+import { MarkingServicePropertiesModule } from '../marking-service-properties/marking-service-properties.module';
 
 @Module({
   imports: [
+    MarkingServicePropertiesModule,
     TypeOrmModule.forFeature([MarkedServicePrice])
   ],
   controllers: [MarkedServicePricesController],
