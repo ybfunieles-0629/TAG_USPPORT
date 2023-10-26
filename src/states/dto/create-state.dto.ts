2740be1e95ae3e4bcbbb8a8c51f99d96de0550bd
Yeattt <nekz.8896@gmail.com>
2023-10-26 17:05:56 -0500

    Create state dto added

--- src/states/dto/create-state.dto.ts
+++ src/states/dto/create-state.dto.ts
@@ -1,1 +1,18 @@
-export class CreateStateDto {}
+import { IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateStateDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  process: string;
+
+  @IsString()
+  @IsUUID()
+  createdBy: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUUID()
+  updatedBy: string;
+}
