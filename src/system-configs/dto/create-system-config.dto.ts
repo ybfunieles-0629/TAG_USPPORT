288bcfbc08559e3cc34c658531ef141d4a9e21ef
Yeattt <nekz.8896@gmail.com>
2023-11-15 15:05:45 -0500

    Dto added

--- src/system-configs/dto/create-system-config.dto.ts
+++ src/system-configs/dto/create-system-config.dto.ts
@@ -1,1 +1,27 @@
-export class CreateSystemConfigDto {}
+import { IsInt, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateSystemConfigDto {
+  @IsInt()
+  generalDeliveryTime: number;
+
+  @IsInt()
+  noCorporativeClientsMargin: number;
+
+  @IsInt()
+  importationFee: number;
+
+  @IsInt()
+  withholdingAtSource: number;
+
+  @IsInt()
+  maxDiscount: number;
+
+  @IsString()
+  @IsUUID()
+  createdBy: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUUID()
+  updatedBy?: string;
+}
