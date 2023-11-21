453184481b98143fb237c6a21579d2f17917bc21
Yeattt <nekz.8896@gmail.com>
2023-11-21 09:23:03 -0500

    Auto load entities activated

--- src/app.module.ts
+++ src/app.module.ts
@@ -70,8 +70,8 @@ import { SupplierPurchaseOrdersModule } from './supplier-purchase-orders/supplie
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      // autoLoadEntities: true,
-      // synchronize: true
+      // synchronize: true,
+      autoLoadEntities: true,
     }),
     ClientsModule,
     UsersModule,
