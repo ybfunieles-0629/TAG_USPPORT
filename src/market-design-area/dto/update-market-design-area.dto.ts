170bbaa2b195b298f1cf01587776394e4f2aefae
yeison-bf <yeison.barrios.funieles@gmail.com>
2023-11-20 20:58:12 -0500

    ok

--- src/market-design-area/dto/update-market-design-area.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMarketDesignAreaDto } from './create-market-design-area.dto';
-
-export class UpdateMarketDesignAreaDto extends PartialType(CreateMarketDesignAreaDto) {}
