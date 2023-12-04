dc03080e004401bec1620fe2c8a8f81b44634575
Yeattt <nekz.8896@gmail.com>
2023-12-04 17:05:08 -0500

    Is pending added in filter reference by is allowed method

--- src/ref-products/ref-products.service.ts
+++ src/ref-products/ref-products.service.ts
@@ -452,12 +452,13 @@ export class RefProductsService {
       ],
     });
 
-    const refProductsToShow: RefProduct[] = [];
+    const refProductsToShow = [];
 
     for (const refProduct of refProducts) {
       if (refProduct.isAllowed === 0 || refProduct.products.some(product => product.isAllowed === 0)) {
         refProductsToShow.push({
           ...refProduct,
+          isPending: 0,
           products: refProduct.products.filter(product => product.isAllowed === 0),
         });
       };
@@ -467,7 +468,6 @@ export class RefProductsService {
 
     return {
       count: paginatedRefProducts.length,
-      isPending: 0,
       paginatedRefProducts
     };
   }
