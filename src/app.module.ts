85560efbfcf632d59cfc8b586b3b969ca5ba5fe1
yeison-bf <yeison.barrios.funieles@gmail.com>
2023-12-13 16:31:21 -0500

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -67,7 +67,7 @@ import { SupplierPurchaseOrdersModule } from './supplier-purchase-orders/supplie
       type: 'mysql',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
+      database: process.env.DB_NAME, 
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       // synchronize: true,
