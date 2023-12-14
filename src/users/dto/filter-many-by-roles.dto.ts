e8a75f2351c8405c401e6f57439ba3e628a84bd8
Yeattt <nekz.8896@gmail.com>
2023-12-14 17:15:14 -0500

    New dto added

--- src/users/dto/filter-many-by-roles.dto.ts
+++ src/users/dto/filter-many-by-roles.dto.ts
@@ -1,7 +1,11 @@
-import { IsArray, IsString } from 'class-validator';
+import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class FilterManyByRolesDto {
   @IsArray()
   @IsString({ each: true })
   roles: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  isCommercial?: boolean;
 };
