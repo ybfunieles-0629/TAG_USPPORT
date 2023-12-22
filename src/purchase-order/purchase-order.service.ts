ce3faa38d33ea032c7a1b826d931c06108e97d42
Yeattt <nekz.8896@gmail.com>
2023-12-22 16:21:55 -0500

    Bugs fixed

--- src/purchase-order/purchase-order.service.ts
+++ src/purchase-order/purchase-order.service.ts
@@ -170,9 +170,6 @@ export class PurchaseOrderService {
       where: {
         id: purchaseOrder.commercialUser,
       },
-      relations: [
-        'user',
-      ],
     });
 
     const clientUser: Client = await this.clientRepository.findOne({
