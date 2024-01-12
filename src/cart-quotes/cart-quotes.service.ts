d10a050992f994126d6b9fd9740b6fed5fe45776
Yeattt <nekz.8896@gmail.com>
2024-01-12 11:50:22 -0500

    Bug fixed

--- src/cart-quotes/cart-quotes.service.ts
+++ src/cart-quotes/cart-quotes.service.ts
@@ -251,8 +251,7 @@ export class CartQuotesService {
         state: cartQuote.state?.name || '',
         products: cartQuote.quoteDetails.map((quoteDetail: QuoteDetail) => {
           return {
-            id: quoteDetail.product.id,
-            refProuctId: quoteDetail.product.refProduct.id,
+            id: quoteDetail.id,
             name: quoteDetail.product.refProduct.name,
             discount: quoteDetail.discount,
             iva: quoteDetail.iva,
