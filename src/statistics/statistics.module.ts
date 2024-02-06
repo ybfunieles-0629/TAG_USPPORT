5c18ea052a9e8bb2b07369ee3e459067936187e9
Yeattt <nekz.8896@gmail.com>
2024-02-06 16:27:24 -0500

    Bug fixed

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -5,10 +5,12 @@ import { StatisticsController } from './statistics.controller';
 import { SystemConfigsModule } from '../system-configs/system-configs.module';
 import { PurchaseOrderModule } from '../purchase-order/purchase-order.module';
 import { ClientsModule } from '../clients/clients.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     ClientsModule,
+    UsersModule,
     SystemConfigsModule,
     PurchaseOrderModule,
   ],
