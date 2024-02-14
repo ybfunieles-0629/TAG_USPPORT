6cfd26059b067fdb9793edab27d58c1a5c670721
Yeattt <nekz.8896@gmail.com>
2024-02-14 18:30:36 -0500

    Bug fixed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -844,7 +844,7 @@ export class UsersService {
 
     const paginatedResults = usersToShow.map((user) => {
       let isAdmin = false;
-      if (user.roles.some(role => role.name.toLowerCase() === 'administrador' || role.name.toLowerCase() === 'super-administrador')) {
+      if (user?.roles?.some(role => role?.name?.toLowerCase() === 'administrador' || role?.name?.toLowerCase() === 'super-administrador')) {
         isAdmin = true;
       }
       return { ...user, isAdmin };
