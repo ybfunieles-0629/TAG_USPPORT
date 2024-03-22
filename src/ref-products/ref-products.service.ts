3f1471eec29bf7a4be691b1026a12ebca22b628e
Yeison Barrios F <yeison.barrios.funieles@gmail.com>
2024-03-22 16:24:15 -0500

    ok

--- src/ref-products/ref-products.service.ts
+++ src/ref-products/ref-products.service.ts
@@ -235,7 +235,7 @@ export class RefProductsService {
       //   100000, 200000,
       // ];
       staticQuantities = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100,
+        1, 2, 3, 
       ];
     } else {
       staticQuantities = [1];
