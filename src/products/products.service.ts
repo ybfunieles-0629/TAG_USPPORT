28b5fe91c024e4ffdbe6368a89ab36b943c6978d
Yeison Barrios F <yeison.barrios.funieles@gmail.com>
2024-05-20 16:23:13 -0500

    ok

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -492,11 +492,11 @@ export class ProductsService {
             ? item.inventario[0].cantidad || 0
             : 0;
         
-        const colorProducts: CategorySupplier = await this.colorRepository.findOne({
-          where: {
-            name: Like(`%${material.color_nombre}%`)
-          }
-        });
+        // const colorProducts: CategorySupplier = await this.colorRepository.findOne({
+        //   where: {
+        //     name: Like(`%${material.color_nombre}%`)
+        //   }
+        // });
 
 
         const newProduct = {
