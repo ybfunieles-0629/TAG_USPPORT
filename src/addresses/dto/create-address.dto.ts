43c5917f719e30232b23a2de383753c3aaa90acb
Yeison Barrios F <yeison.barrios.funieles@gmail.com>
2024-06-12 19:14:47 -0500

    ok

--- src/addresses/dto/create-address.dto.ts
+++ src/addresses/dto/create-address.dto.ts
@@ -1,32 +1,40 @@
-import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsString, IsOptional } from 'class-validator';
 
 export class CreateAddressDto {
 
-  
+  @IsOptional()
   @IsString()
   clientId: string;
   
+  @IsOptional()
   @IsString()
   country: string;
 
+  @IsOptional()
   @IsString()
   city: string;
 
+  @IsOptional()
   @IsString()
   address: string;
 
+  @IsOptional()
   @IsString()
   postalCode: string;
 
+  @IsOptional()
   @IsString()
   gpsLocation: string;
 
+  @IsOptional()
   @IsString()
   deliveryAddress: string;
 
+  @IsOptional()
   @IsString()
   mainAddress: string;
 
+  @IsOptional()
   @IsNumber()
   isPrimary: number;
 
